5d867aa55483f05755a10bee2931ba4712a41c74
Alvin Ashiatey <alvinashiatey@gmail.com>
2023-08-29 12:10:47 -0400

    programs

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,5 +3,7 @@ import category from './category'
 import post from './post'
 import settings from './settings'
 import artist from './artist'
+import program from './program'
+import tags from './tags'
 
-export const schemaTypes = [post, artist, category, blockContent, settings]
+export const schemaTypes = [program, artist, post, tags, category, blockContent, settings]
---
+++ schemas/program.ts
@@ -0,0 +1,150 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'program',
+  title: 'Program',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'startDateTime',
+      title: 'Start Date/Time',
+      type: 'datetime',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'endDateTime',
+      title: 'End Date/Time',
+      type: 'datetime',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'coverImage',
+      title: 'Cover Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          title: 'Alt Text',
+          type: 'string',
+        },
+      ],
+    }),
+    defineField({
+      name: 'pressKit',
+      title: 'Press Kit',
+      type: 'file',
+    }),
+    defineField({
+      name: 'curator',
+      title: 'Curator',
+      type: 'string',
+    }),
+    defineField({
+      name: 'artists',
+      title: 'Artists',
+      type: 'array',
+      of: [
+        {
+          name: 'artist',
+          title: 'Artist',
+          type: 'reference',
+          to: [{type: 'artist'}],
+        },
+      ],
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+          marks: {
+            annotations: [
+              {
+                name: 'link',
+                type: 'object',
+                title: 'External Link',
+                fields: [
+                  {
+                    name: 'href',
+                    type: 'url',
+                    title: 'URL',
+                  },
+                  {
+                    title: 'Open in new tab',
+                    name: 'blank',
+                    description: 'Read https://css-tricks.com/use-target_blank/',
+                    type: 'boolean',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [
+        {
+          name: 'image',
+          title: 'Image',
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              name: 'alt',
+              title: 'Alt Text',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    }),
+    defineField({
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        {
+          name: 'tag',
+          title: 'Tag',
+          type: 'reference',
+          to: [{type: 'tag'}],
+        },
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'coverImage',
+    },
+  },
+})
